c68a987d41815b3c17f5c0cc18f5a0f0c598ca73
monish karunakaran <monish.karunakaran@quicksort.dev>
2021-03-07 17:50:58 +0530

    notes and understanding actions,reducer,store,dispatch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import reactDom from 'react-dom'
+import App from './App'
+import {createStore} from "redux"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// action => describes what you want to do
+const increment=()=>{
+    return {
+        type:"Increment",
+    }
+}
+
+const decrement=()=>{
+    return {
+        type:"Decrement",
+    }
+}
+
+// reducer => explains what happens to the state on action
+const counter=(state=0,action)=>{
+    switch(action.type){
+        case "Increment": return state+1;
+        case "Decrement":return state-1;
+    }
+}
+
+// store => is a global state
+
+let store=createStore(counter)
+
+store.subscribe(()=>{console.log(store.getState())})
+// Dispatch =>execute the actio to reducer
+store.dispatch(increment())
+
+
+
+reactDom.render(<App/>,document.getElementById("root"))
